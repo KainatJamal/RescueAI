86422626aba231731e8b57208d915013d64e2054
Kainat Jamal <kainat.jamal2@gmail.com>
2026-02-28 18:22:13 +0500

    FYP backend

---
+++ RescueAI_API/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace RescueAI_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
